5da6d269ecbf155598ebb84283a0a1797915c845
Kevin Edington <kevin.edington@snapito.com>
2018-08-02 11:15:10 -0700

    Intial Commit

---
+++ Example/Pods/Target Support Files/Pods-SMJJSONPath_Tests/Pods-SMJJSONPath_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SMJJSONPath_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SMJJSONPath_Tests
+@end
---
+++ Example/Pods/Target Support Files/SMJJSONPath/SMJJSONPath-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SMJJSONPath : NSObject
+@end
+@implementation PodsDummy_SMJJSONPath
+@end
